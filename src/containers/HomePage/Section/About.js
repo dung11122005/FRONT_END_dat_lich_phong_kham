29a398adfdfa9bd57a7c937abe8f3703ebf1388e
dung11122005 <dungakaishi900@gmail.com>
2024-06-01 09:47:53 +0700

    1

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -23,9 +23,9 @@ class About extends Component {
                 <div className='section-about-content'>
                     <div className='content-left'>
                         <iframe width="100%" height="400px" src="https://www.youtube.com/embed/21tjOW8BvB4?list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI"
-                            title="Bài N5: Demo Sản Phẩm Đạt Được Khi Kết Thúc Khóa Học Trên Production |Khóa Học Node.js và React" frameborder="0"
+                            title="Bài N5: Demo Sản Phẩm Đạt Được Khi Kết Thúc Khóa Học Trên Production |Khóa Học Node.js và React" frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
 
                         </iframe>
                     </div>
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -20,8 +20,16 @@ const userPersistConfig = {
     key: 'user',
     whitelist: ['isLoggedIn', 'userInfo']
 };
+
+const appPersisConfig = {
+    ...persistCommonConfig,
+    key: 'app',
+    whitelist: ['language']
+}
+
+
 export default (history) => combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer
+    app: persistReducer(appPersisConfig, appReducer)
 })
